c7bd0c0dfbebdd8682ccf73994f81828ff793139
Rahma Ashraf <rahmaashraf.dev@gmail.com>
2025-10-15 02:23:40 +0300

    initial commit

---
+++ lib/app_router.dart
@@ -0,0 +1,11 @@
+import 'package:book_finder_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:book_finder_app/app_router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp(appRouter: AppRouter()));
+}
+class MyApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const MyApp({super.key , required this.appRouter});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.onGenerateRoute,
+      title: 'Book Finder App',
+    );
+  }
+}
